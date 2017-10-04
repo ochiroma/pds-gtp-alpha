ab32074de2a61ee448cb334c132051727baa6a79
Onkar Chiroma <info@onkarchiroma.co.uk>
2017-10-04 18:32:06 +0100

    Add if_eq helper and impliment on 'what next' section for 'b' subtopic journey

--- build.js
+++ build.js
@@ -65,6 +65,13 @@ handlebars.registerHelper("cleanURL", function(input) {
     return output;
 });
 
+handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a == b) // Or === depending on your needs
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
 
 
 handlebars.registerHelper('marked', function (text) {
