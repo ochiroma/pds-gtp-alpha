97cc7fe778e7f8e715969ac195830803ea090e77
Onkar Chiroma <info@onkarchiroma.co.uk>
2017-10-31 15:44:31 +0000

    Add 404

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const app = express();
 
 app.use(express.static('build'));
 
+app.use(function (req, res, next) {
+  res.status(404).redirect("/errors/404")
+})
+
 app.listen(process.env.PORT || 3000, function () {
   console.log('App started :)')
 })
---
+++ src/errors/404/index.md
@@ -0,0 +1,5 @@
+---
+contentful:
+  entry_id: 5yYcuDvKpya4ciko0S8OCe
+layout: 404.html
+---
